0c9b774a108a6c976dc0bdc6b0816f9a4c10fc20
ONHN <ONHN@DK01WV6164>
2022-06-02 19:54:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentValueCalculator_HW2
+{
+    enum TransactionType
+    {
+        Buy,
+        Sell
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+

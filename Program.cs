da8abe7abe849e0dce145e15e14f4a24a55e1424
ONHN <ONHN@DK01WV6164>
2022-06-02 20:00:05 +0200

    Created Current value Calculator

--- Program.cs
+++ Program.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace CurrentValueCalculator_HW2
 {
-    enum TransactionType
-    {
-        Buy,
-        Sell
+    enum TransactionType {
+        Buy, Sell
     }
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Input the nominal of the trade:");
+            string userInput = Console.ReadLine();
+            int nominal = int.Parse(userInput ?? "0");
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
+            Console.WriteLine("Input the trade price of the deal:");
+            userInput = Console.ReadLine();
+            double tradePrice = double.Parse(userInput ?? "0");
 
+            Console.WriteLine("Input the transaction type:");
+            userInput = Console.ReadLine();
+            TransactionType trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput ?? "0", true);
+
+            int sign = trcType == TransactionType.Buy ? 1 : -1;
+            double currentValue = sign * nominal * tradePrice;
+            Console.WriteLine($"The current value of a deal is: {currentValue}");
+
+            Console.ReadLine();
         }
     }
 }
